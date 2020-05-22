94365e91d8daceb0fb1813806af6b1beb278cf0e
Felix Ayala <felix1262@gmail.com>
2020-05-22 15:24:21 -0400

    Rename migration files

--- src/RegionsServiceProvider.php
+++ src/RegionsServiceProvider.php
@@ -20,9 +20,14 @@ class RegionsServiceProvider extends ServiceProvider
 
     public function publishResources()
     {
-        $this->publishes([
-            __DIR__ . '/../database/migrations' => database_path('migrations')
-        ], 'paraguay-regions-migrations');
+        if (! class_exists('CreateDepartmentsTable') || ! class_exists('CreateCitiesTable')) {
+            $timestamp = date('Y_m_d_His', time());
+
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_departments_table.php' => database_path("/migrations/{$timestamp}_create_departments_table.php"),
+                __DIR__.'/../database/migrations/create_cities_table.php' => database_path("/migrations/{$timestamp}_create_cities_table.php"),
+            ], 'paraguay-regions-migrations');
+        }
 
         $this->publishes([
             __DIR__ . '/../database/seeds' => database_path('seeds')
