47a01204fdf42f390bd6f68bade2ce477a353ef8
Felix Ayala <felix1262@gmail.com>
2020-05-22 15:36:43 -0400

    Merge database migration file

--- database/migrations/create_departments_and_cities_table.php
+++ database/migrations/create_departments_and_cities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCitiesTable extends Migration
+class CreateDepartmentsAndCitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,13 @@ class CreateCitiesTable extends Migration
      */
     public function up()
     {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('capital');
+            $table->timestamps();
+        });
+
         Schema::create('cities', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('department_id')->unsigned();
--- src/RegionsServiceProvider.php
+++ src/RegionsServiceProvider.php
@@ -20,12 +20,11 @@ class RegionsServiceProvider extends ServiceProvider
 
     public function publishResources()
     {
-        if (! class_exists('CreateDepartmentsTable') || ! class_exists('CreateCitiesTable')) {
+        if (! class_exists('CreateDepartmentsAndCitiesTable')) {
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__.'/../database/migrations/create_departments_table.php' => database_path("/migrations/{$timestamp}_create_departments_table.php"),
-                __DIR__.'/../database/migrations/create_cities_table.php' => database_path("/migrations/{$timestamp}_create_cities_table.php"),
+                __DIR__.'/../database/migrations/create_departments_and_cities_table.php' => database_path("/migrations/{$timestamp}_create_departments_and_cities_table.php"),
             ], 'paraguay-regions-migrations');
         }
 
